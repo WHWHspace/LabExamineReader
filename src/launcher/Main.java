e32953ee77eb6f0d9f84393496a32cdc16d2dc23
yuan binhua <yuan binhua>
2016-06-13 14:21:33 +0800

    修改检验分类
    Signed-off-by: yuan binhua <yuan binhua>

--- src/launcher/Main.java
+++ src/launcher/Main.java
@@ -32,7 +32,7 @@ public class Main {
         Runnable runnable = new LabExamineReader(lastReadTime);
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         // 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
-        service.scheduleAtFixedRate(runnable, 0, INTERVAL, TimeUnit.SECONDS);
+        service.scheduleAtFixedRate(runnable, 1, INTERVAL, TimeUnit.SECONDS);
     }
 
     /**
